13119daae949d2814667a41ad445cbc1b78b0804
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-21 16:45:12 +0530

    minor change in syntax

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
-const {
-    MONGO_URI
-} = process.env;
+
 const connectDB = async() => {
     try{
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("mongoDB connected");
     }catch(error){
         console.log(error);
