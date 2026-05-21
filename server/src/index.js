9877da549c15cc56df040e0b14f862a01de29c25
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-21 14:17:11 +0530

    connectDB added

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
 
+dotenv.config();
 
-
-const port = 4000;
+const port = process.env.port || 5000;
 
 const app = express();
+connectDB()
 
 app.use(cors());
 app.use(express.json());
