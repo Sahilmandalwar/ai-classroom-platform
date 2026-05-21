72a3592fc1ff378273b3b45dedca7ea4856d9e8a
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-21 16:42:37 +0530

    CreateBrowserRouter added and routing handled

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Login from './pages/login.jsx';
+import Signup from './pages/signup.jsx';
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />
+  },
+  {
+    path:"/login",
+    element:<Login />
+  },
+  {
+    path:"/signup",
+    element:<Signup />
+  }
+])
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+      
+    
   </StrictMode>,
-)
+);
