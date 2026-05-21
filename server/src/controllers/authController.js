efc047667d611ef1703d5a080ef4a0d6c4cd4eed
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-21 16:38:00 +0530

    bug fix --required await added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -40,7 +40,7 @@ const signup = async(req, res) => {
 
 const login = async(req, res) => {
     try{
-        const {name, email, password} = req.body;
+        const {email, password} = req.body;
 
         const user = await User.findOne({email});
 
@@ -50,7 +50,7 @@ const login = async(req, res) => {
             })
         }
 
-        const isMatch = bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
 
         if(!isMatch) {
              return res.status(400).json({
@@ -64,7 +64,7 @@ const login = async(req, res) => {
         )
 
         res.status(200).json({
-            token, user
+             user, token
         })
 
 
