b59880acd29fe2affbcb3ca983ddc83463c21f0a
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-21 15:01:04 +0530

    authRoutes is added

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ connectDB()
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/v1/api/auth',authRoutes)
 
 app.get("/",(req, res)=>{
     res.send("Welcome To AI Classroom Platform");
