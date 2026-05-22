e5337c8ccd429dd8ef34e29b1bc6b2d283fad2d4
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-22 17:01:01 +0530

    authProvider context added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,13 @@ import './index.css'
 
 import { RouterProvider} from 'react-router-dom';
 import router from './routes/appRouter';
-
-
+import { AuthProvider } from './contexts/authContext.jsx';
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-      
-    
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
