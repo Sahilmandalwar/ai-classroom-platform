abb686fb47ebbe08bbf4d475837bc92bcbbd9b86
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-22 16:58:39 +0530

    protectedRoute component integrated

---
+++ client/src/components/protectedRoute.jsx
@@ -0,0 +1,13 @@
+import {Navigate} from 'react-router-dom';
+import { useAuth } from '../contexts/authContext';
+
+const ProtectedRoute = ({children}) => {
+    const {isAuthenticated} = useAuth();
+    
+    if(!isAuthenticated) {
+        return <Navigate to="/login"  />
+    }
+    return children;
+}
+
+export default ProtectedRoute;
