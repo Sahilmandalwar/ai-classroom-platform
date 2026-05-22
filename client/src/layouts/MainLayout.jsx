b94989d6cdbd0c0c6cedd3c7b02ed57be2da734a
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-22 16:54:54 +0530

    Navbar component added

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import {Outlet} from 'react-router-dom';
+import Navbar from '../components/navbar.jsx';
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-black">
+      <Navbar />
       <Outlet />
     </div>
   )
