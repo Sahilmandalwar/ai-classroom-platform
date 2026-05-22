21b4a00a91c7748251fc4b5358dff328ca446ea1
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-22 14:48:01 +0530

    centralized authentication file

---
+++ client/src/services/authServices.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL:"http://localhost:5000/v1/api/auth",
+});
+
+export const signUpUser = async (userData) => {
+    const response = await API.post('/signup', userData);
+    return response.data;
+}
+
+export const loginUser = async (userData) => {
+    const response = await API.post('/login', userData);
+    return response.data;
+}
