435c3cbc86ad96cf8f0ff076cfd725e2e4132d48
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-23 18:20:30 +0530

    classroom route added

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-
+import classroomRoutes from "./routes/classroomRoute.js";
 dotenv.config();
 
 const port = process.env.port || 5000;
@@ -14,7 +14,8 @@ connectDB()
 app.use(cors());
 app.use(express.json());
 
-app.use('/v1/api/auth',authRoutes)
+app.use('/v1/api/auth',authRoutes);
+app.use('/v1/api/classroom', classroomRoutes);
 
 app.get("/",(req, res)=>{
     res.send("Welcome To AI Classroom Platform");
