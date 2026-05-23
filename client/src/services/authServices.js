90dd7fc546c9729ba35c6d6988b909f41d9ed51e
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-05-23 18:29:59 +0530

    current user fetching added

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-
-const API = axios.create({
-    baseURL:"http://localhost:5000/v1/api/auth",
-});
+import API from "./api";
 
 export const signUpUser = async (userData) => {
-    const response = await API.post('/signup', userData);
+    const response = await API.post('/auth/signup', userData);
     return response.data;
 }
 
 export const loginUser = async (userData) => {
-    const response = await API.post('/login', userData);
-    
+    const response = await API.post('/auth/login', userData)
+    return response.data;
+}
+
+export const getCurrentUser = async () => {
+    const response = await API.get("/auth/me");
     return response.data;
 }
